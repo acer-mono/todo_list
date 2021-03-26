729e05c25d574da07b03afc807cccee599b58738
IvanovaMV <margaritastar92@gmail.com>
2021-03-26 14:15:34 +0700

    refactor Counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,12 +7,18 @@ type CounterType = {
 export const Counter = ({ items }: CounterType) => {
   return (
     <div>
-      <span>Всего:</span>
-      <div data-testid="counter-all">{items.length}</div>
-      <span>Выполнено:</span>
-      <div data-testid="counter-done">{items.filter(el => el.isDone).length}</div>
-      <span>В процессе:</span>
-      <div data-testid="counter-not-done">{items.filter(el => !el.isDone).length}</div>
+      <div>
+        <span>All: </span>
+        <span data-testid="counter-all">{items.length}</span>
+      </div>
+      <div>
+        <span>Done: </span>
+        <span data-testid="counter-done">{items.filter(el => el.isDone).length}</span>
+      </div>
+      <div>
+        <span>Not done: </span>
+        <span data-testid="counter-not-done">{items.filter(el => !el.isDone).length}</span>
+      </div>
     </div>
   );
 };
