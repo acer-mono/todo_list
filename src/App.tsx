28ffba92281ac84062d06b7db0db4dd96e4b0754
IvanovaMV <margaritastar92@gmail.com>
2021-04-02 13:09:16 +0700

    remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useState, Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { List } from './components/List/List';
 import { SearchPanel } from './components/SearchPanel/SearchPanel';
 import { CreateForm } from './components/CreateForm/CreateForm';
 import { CategorySelect } from './components/CategorySelect/CategorySelect';
 import { selectErrors } from './redux/selectors';
-import { AuthCheck, useUser } from 'reactfire';
-import LogInForm from './LoginForm';
-import firebase from 'firebase';
-import 'firebase/auth';
 import { Alert } from './components/Alert/Alert';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearErrors } from './redux/actions';
@@ -18,52 +14,28 @@ import { ITEM_STATE_FILTER } from './redux/reducers/todos';
 function App() {
   const messages = useSelector(selectErrors);
   const dispatch = useDispatch();
-  const db = firebase.database();
-  const [user, setUser] = useState(useUser().data);
 
-  useEffect(() => {
-    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(newUser =>
-      setUser(() => {
-        if (newUser) {
-          db.ref(`stores/${newUser.uid}`).on('value', () => {
-            //const value = snapshot.val()?.store;
-            /*
-            if (!value?.list) {
-              setStore(initialState);
-            } else {
-              setStore(value);
-            }
-             */
-          });
-          return newUser;
-        }
-        return user;
-      })
-    );
-    return () => unregisterAuthObserver();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
-    <Suspense fallback="Loading...">
-      <AuthCheck fallback={<LogInForm />}>
-        <Alert messages={messages} delay={3000} onClose={() => dispatch(clearErrors({}))} />
-        <div className="wrapper">
-          <div>
-            <h1>Список дел</h1>
-            <h2>Лабораторная №2. Добавляем элемент в список</h2>
-          </div>
-          <div>
-            <CreateForm />
-            <CategorySelect filterValues={ITEM_STATE_FILTER} />
-            <br />
-            <SearchPanel />
-            <br />
-            <Counter />
-            <List />
-          </div>
+    <>
+      <Alert messages={messages} delay={3000} onClose={() => dispatch(clearErrors({}))} />
+      <div className="wrapper">
+        <div>
+          <h1>Список дел</h1>
+          <h2>Лабораторная №2. Добавляем элемент в список</h2>
         </div>
-      </AuthCheck>
-    </Suspense>
+        <div>
+          <CreateForm />
+          <CategorySelect filterValues={ITEM_STATE_FILTER} />
+          <br />
+          <SearchPanel />
+          <br />
+          <Counter />
+          <List />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { FirebaseAppProvider } from 'reactfire';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import firebaseConfig from './firebaseConfig.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </FirebaseAppProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
