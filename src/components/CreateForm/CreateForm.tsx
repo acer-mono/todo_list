9a16f1fe9ed46288e97ceb44959f9394315c1d36
IvanovaMV <margaritastar92@gmail.com>
2021-04-06 14:37:54 +0700

    try connect to api

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -1,54 +1,37 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addError, create } from '../../redux/actions';
-import { selectListTitles } from '../../redux/selectors';
 
 export const CreateForm = () => {
   const [name, setName] = useState('');
   const [position, setPosition] = useState(0);
   const dispatch = useDispatch();
-  const todos = useSelector(selectListTitles);
 
-  /*
-  const uid = function () {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
-  };
-  */
-
-  function validate() {
-    if (name === '') {
-      dispatch(addError({ error: 'Название задачи не может быть пустым' }));
-      return false;
-    }
-    if (todos.includes(name)) {
-      dispatch(addError({ error: 'Названия задач не должны повторяться' }));
-      return false;
-    }
-    return true;
-  }
   const url = 'http://localhost:3001/todos';
 
   const createListItem = async () => {
-    if (validate()) {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: name,
-          position: position
-        })
-      });
-      const data = await response.json();
-      if (data.error) {
-        dispatch(addError({ error: data.error }));
-      } else {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        position: position
+      })
+    });
+    await response
+      .json()
+      .then(data => {
+        if (data.error) {
+          dispatch(addError({ error: data.error }));
+          return;
+        }
+        dispatch(create({ item: data }));
         setPosition(position => position + 1);
         setName('');
-        dispatch(create({ item: data }));
-      }
-    }
+      })
+      .catch(error => dispatch(addError({ error: error.error })));
   };
   return (
     <>
