6678a0bd14bcd8b26ea64e1e0705bb531fc9c95e
acer-mono <margaritastar92@gmail.com>
2021-04-13 22:03:32 +0700

    clear title after success

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addElement, REQUEST_STATUS } from '../../redux/actions';
+import { addElement, REQUEST_STATUS, setRequestStatus } from '../../redux/actions';
 import { Store } from '../../redux/reducers/todos';
 
 export const CreateForm = () => {
   const [name, setName] = useState('');
   const requestState = useSelector((state: Store) => state.requestStatus);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (requestState == REQUEST_STATUS.SUCCESS) {
+      setName('');
+      console.log(requestState);
+      dispatch(setRequestStatus(REQUEST_STATUS.IDLE));
+    }
+  }, [requestState]);
 
   const createListItem = async () => {
     dispatch(addElement(name));
