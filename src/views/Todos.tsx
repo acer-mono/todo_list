492867a5d33a1fae8f332640ff38460dc2f99b52
IvanovaMV <margaritastar92@gmail.com>
2021-04-27 11:30:09 +0700

    add logout button

--- src/views/Todos.tsx
+++ src/views/Todos.tsx
@@ -3,12 +3,17 @@ import { SearchPanel } from '../components/SearchPanel/SearchPanel';
 import { List } from '../components/List/List';
 import { ITEM_STATE_FILTER } from '../redux/reducers/filter';
 import React from 'react';
-import pin from './pin.png';
+import { FaSignOutAlt } from 'react-icons/all';
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/actions';
 
 export const Todos = () => {
+  const dispatch = useDispatch();
   return (
     <div className="todo-wrapper">
-      <img className="pin" src={pin} alt="pin" />
+      <button className="button-close" onClick={() => dispatch(logout())}>
+        <FaSignOutAlt />
+      </button>
       <div className="content-main">
         <div className="header-main">Todo List</div>
         <div className="search-form">
